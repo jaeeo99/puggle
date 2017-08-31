98265c7189ca48461282a2468d41a9ed886aeeb2
박진철 <jaeeo@cedar.kr>
2017-08-31 21:00:52 +0900

    HorizontalCalendar add to MainActivity

--- puggle/src/main/java/com/puggle/magic/puggle/MainActivity.java
+++ puggle/src/main/java/com/puggle/magic/puggle/MainActivity.java
@@ -1,29 +1,36 @@
 package com.puggle.magic.puggle;
 
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
-import android.icu.util.Calendar;
-import android.icu.util.TimeZone;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
 
 import com.puggle.magic.puggle.calendar.horizontal.HorizontalCalendar;
-import com.puggle.magic.puggle.calendar.horizontal.HorizontalCalendarView;
 
+import java.util.Calendar;
 import java.util.Locale;
+import java.util.TimeZone;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.calendarView)
-    HorizontalCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
         TimeZone seoulTime = TimeZone.getTimeZone("Asia/Seoul");
 
         Calendar endDate = Calendar.getInstance(seoulTime);
@@ -32,19 +39,20 @@ public class MainActivity extends AppCompatActivity {
         Calendar startDate = Calendar.getInstance(seoulTime);
         startDate.add(Calendar.MONTH, -1);
 
-        HorizontalCalendar calendarView = new HorizontalCalendar.Builder(this, R.id.calendarView)
+        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(findViewById(R.id.horizontalCalendar), R.id.calendarView)
                 .startDate(startDate.getTime())
                 .endDate(endDate.getTime())
                 .datesNumberOnScreen(7)   // Number of Dates cells shown on screen (Recommended 5)
-                .dayNameFormat("E")	  // WeekDay text format
+                .dayNameFormat("E")      // WeekDay text format
                 .dayNumberFormat("d")    // Date format
                 .dayNameLocale(Locale.KOREA)
                 .dayNumberLocale(Locale.KOREA)
-                .showDayName(true)	  // Show or Hide dayName text
-                .showMonthName(false)	  // Show or Hide month text
+                .showDayName(true)      // Show or Hide dayName text
+                .showMonthName(false)      // Show or Hide month text
                 .textColor(Color.LTGRAY, Color.WHITE)    // Text color for none selected Dates, Text color for selected Date.
                 .selectedDateBackground(Color.TRANSPARENT)  // Background color of the selected date cell.
                 .selectorColor(Color.RED)   // Color of the selection indicator bar (default to colorAccent).
                 .build();
     }
+
 }
--- puggle/src/main/java/com/puggle/magic/puggle/fragment/HorizontalCalendarFragment.java
+++
@@ -1,8 +0,0 @@
-package com.puggle.magic.puggle.fragment;
-
-/**
- * Created by jaeeo99 on 2017. 8. 6..
- */
-
-public class HorizontalCalendarFragment {
-}
