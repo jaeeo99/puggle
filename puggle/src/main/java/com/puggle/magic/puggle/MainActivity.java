78deef3e5d1b8a0b3b21aef9ccfd38b2634939a0
박진철 <jaeeo@cedar.kr>
2018-02-02 21:53:35 +0900

    magicapp move to directcallapp

--- puggle/src/main/java/com/puggle/magic/puggle/MainActivity.java
+++ puggle/src/main/java/com/puggle/magic/puggle/MainActivity.java
@@ -1,58 +1,179 @@
 package com.puggle.magic.puggle;
 
-import android.annotation.TargetApi;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.renderscript.Allocation;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-import com.puggle.magic.puggle.calendar.horizontal.HorizontalCalendar;
+import com.mikepenz.iconics.context.IconicsLayoutInflater;
 
-import java.util.Calendar;
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isCalled = false;
+    private String callTo = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory(getLayoutInflater(), new IconicsLayoutInflater(getDelegate()));
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
         setContentView(R.layout.activity_main);
+        setDate();
+        ArrayList<TextView> textViews = getTextViews();
 
-        TimeZone seoulTime = TimeZone.getTimeZone("Asia/Seoul");
 
-        Calendar endDate = Calendar.getInstance(seoulTime);
-        endDate.add(Calendar.MONTH, 1);
-
-        Calendar startDate = Calendar.getInstance(seoulTime);
-        startDate.add(Calendar.MONTH, -1);
-
-        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(findViewById(R.id.horizontalCalendar), R.id.calendarView)
-                .startDate(startDate.getTime())
-                .endDate(endDate.getTime())
-                .datesNumberOnScreen(7)   // Number of Dates cells shown on screen (Recommended 5)
-                .dayNameFormat("E")      // WeekDay text format
-                .dayNumberFormat("d")    // Date format
-                .dayNameLocale(Locale.KOREA)
-                .dayNumberLocale(Locale.KOREA)
-                .showDayName(true)      // Show or Hide dayName text
-                .showMonthName(false)      // Show or Hide month text
-                .textColor(Color.LTGRAY, Color.WHITE)    // Text color for none selected Dates, Text color for selected Date.
-                .selectedDateBackground(Color.TRANSPARENT)  // Background color of the selected date cell.
-                .selectorColor(Color.RED)   // Color of the selection indicator bar (default to colorAccent).
-                .build();
+        JoystickView joystick = findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                for (TextView textView : textViews) {
+                    textView.setTextColor(Color.WHITE);
+                    textView.setTextSize(15);
+                }
+                Log.d("Log", "angle : " + angle + ", strength : " + strength);
+                if(strength == 0 && angle == 0 && isCalled){
+                    call(callTo);
+                    isCalled = false;
+                    callTo = "";
+                }
+                else if (strength > 90 && strength < 101) {
+                    isCalled = false;
+                    if (angle < 45) {
+                        textViews.get(0).setTextColor(Color.BLACK);
+                        textViews.get(0).setTextSize(20);
+                        isCalled = true;
+                        callTo = "010-3477-1507";
+                    } else if (angle < 135) {
+                        textViews.get(1).setTextColor(Color.BLACK);
+                        textViews.get(1).setTextSize(20);
+                        isCalled = true;
+                        callTo = "010-3477-1507";
+                    } else if (angle < 225) {
+                        textViews.get(2).setTextColor(Color.BLACK);
+                        textViews.get(2).setTextSize(20);
+                        isCalled = true;
+                        callTo = "010-3477-1507";
+                    } else if (angle < 315) {
+                        textViews.get(3).setTextColor(Color.BLACK);
+                        textViews.get(3).setTextSize(20);
+                        isCalled = true;
+                        callTo = "010-3477-1507";
+                    } else {
+                        textViews.get(0).setTextColor(Color.BLACK);
+                        textViews.get(0).setTextSize(20);
+                        isCalled = true;
+                        callTo = "010-3477-1507";
+                    }
+                } else {
+                    isCalled = false;
+                }
+            }
+        }, 100);
     }
 
+    public void setDate(){
+        final Handler timeHandler = new Handler(getMainLooper());
+        TextView timeTextView = findViewById(R.id.time);
+        timeHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timeTextView.setText(new SimpleDateFormat("h:mm", Locale.US).format(new Date()));
+                timeHandler.postDelayed(this, 1000);
+            }
+        }, 10);
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M월 d일 E요일", Locale.KOREAN);
+        TextView dateTextView = findViewById(R.id.date);
+        dateTextView.setText(dateFormat.format(today));
+    }
+
+    public ArrayList<TextView> getTextViews(){
+        ArrayList<TextView> textViews = new ArrayList<>();
+        textViews.add(findViewById(R.id.labelRight));
+        textViews.add(findViewById(R.id.labelTop));
+        textViews.add(findViewById(R.id.labelLeft));
+        textViews.add(findViewById(R.id.labelBottom));
+        return textViews;
+    }
+
+    /** Called when the user taps the Send button */
+    public void call(String phoneNum) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int permissionResult = checkSelfPermission(Manifest.permission.CALL_PHONE);
+
+            if (permissionResult == PackageManager.PERMISSION_DENIED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                    dialog.setTitle("권한이 필요합니다.")
+                            .setMessage("이 기능을 사용하기 위해서는 단말기의 \"전화걸기\" 권한이 필요합니다. 계속 하시겠습니까?")
+                            .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1000);
+                                    }
+                                }
+                            })
+                            .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "기능을 취소했습니다", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .create()
+                            .show();
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1000);
+                }
+            } else {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNum));
+                startActivity(intent);
+            }
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNum));
+            startActivity(intent);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-1111-2222"));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    startActivity(intent);
+                }
+            } else {
+                Toast.makeText(MainActivity.this, "권한요청을 거부했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
